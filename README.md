37424bdc2efbd55e0a4f9ed2f15592c6082d6ecf
AGO McAGO <agomcago@gmail.com>
2022-12-04 03:09:11 +0100

    A typical project structure.

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 
-partials: A way to split up multiple codes into different files.
-There could be a file for Variables, CSS Resets, Buttons, Links, Background, Colours, e.t.c.
-
-
-imports: Importing the different files into one root SASS file.
+A typical example of a project structure.
--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,18 @@ const { src, dest, watch, series } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 function buildStyles() {
-  return src('index.scss')
+  /*
+  asterics (i.e. "*") means look for any file with ".scss" extention and compile; multiple SASS files will be compiled.
+  "agoSASS/ ** /*" : this path means find any SASS file, or any subfolder with a SASS file inside the "agoSASS" folder and compile. "**" means subfolder.
+  */
+  return src('agoSASS/**/*.scss')
     .pipe(sass())
     .pipe(dest('css'));
 }
 
 function watchTask() {
-  watch( ['index.scss'], buildStyles );
+  // the asterics (i.e. "*") means this function will watch multiple SASS files.
+  watch( ['agoSASS/**/*.scss'], buildStyles ); // to watch any/all SASS files inside the "agoSASS" folder.
 }
 
 exports.default = series( buildStyles , watchTask );
