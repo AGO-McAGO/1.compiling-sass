eabc4ee09dba3e86b215458e6f31c79e1c6a5d85
AGO McAGO <agomcago@gmail.com>
2022-12-10 16:55:42 +0100

    Customizing the SASS library.

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,16 @@ const sass = require("gulp-sass")(require("sass"));
 const purgecss = require("gulp-purgecss");
 
 function buildStyles() {
-  return src("agoSASS/**/*.scss")
+  // path to build the "index.scss" file inside "custOwnSASS" folder instead of the one inside "agoSASS".
+  return src("custOwnSASS/**/*.scss")
     .pipe( sass() )
     .pipe( purgecss({ content: ["*.html"] }) ) // to look into all(*) files with a ".html" extension to determine css rules that are used and unused.
     .pipe(dest("css"));
 }
 
 function watchTask() {
-  watch( [ "agoSASS/**/*.scss", ".html" ], buildStyles );
+  // path to watch the "index.scss" file inside "custOwnSASS" folder instead of the one inside "agoSASS".
+  watch( [ "custOwnSASS/**/*.scss", "*.html" ], buildStyles );
 }
 
 exports.default = series( buildStyles , watchTask );
